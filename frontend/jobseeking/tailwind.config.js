c65324e7a08379ec038d72d317837b4d621e26ca
kristina <kristina@luday.se>
2025-10-24 13:39:57 +0200

    jopseeking-app

---
+++ frontend/jobseeking/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default { 
+   content: [
+  "./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths as needed
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
